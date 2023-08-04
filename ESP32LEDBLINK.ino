cc86ceee898ab73c3b543b8b6903b8ac550a61d0
leolinkebrown <141324171+leolinkebrown@users.noreply.github.com>
2023-08-04 16:23:00 +1000

    Update ESP32LEDBLINK.ino

--- ESP32LEDBLINK.ino
+++ ESP32LEDBLINK.ino
@@ -1,10 +1,13 @@
 const int LED_PIN = 13; //LED pin is defined
 void setup() {
   pinMode (LED_PIN, OUTPUT);  // Pin 13 is set as OUTPUT
+  Serial.begin(9600);
 }
 void loop() {
   digitalWrite (LED_PIN, HIGH); // LED on
+  Serial.println("LED On!");
   delay(1000);  // delay of 1 sec
   digitalWrite (LED_PIN, LOW);  // LED off
+  Serial.println("LED Off!");
   delay(1000);  // delay of 1 sec
 }
